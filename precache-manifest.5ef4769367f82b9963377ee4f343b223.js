010bc4c0a444de7d133f49155ea235ea9602f8c1
Daniel Morales <danmorales209@gmail.com>
2019-05-05 18:43:36 -0700

    Updates

---
+++ precache-manifest.5ef4769367f82b9963377ee4f343b223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f56f978d0b70d9a7414504bbaa77b2",
+    "url": "/my-clicky-app/index.html"
+  },
+  {
+    "revision": "ea79fc85284e238220fe",
+    "url": "/my-clicky-app/static/css/main.52698ab8.chunk.css"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/my-clicky-app/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "ea79fc85284e238220fe",
+    "url": "/my-clicky-app/static/js/main.7cbec407.chunk.js"
+  },
+  {
+    "revision": "ca374795b70ec6c947de",
+    "url": "/my-clicky-app/static/js/runtime~main.f7788799.js"
+  }
+]);
